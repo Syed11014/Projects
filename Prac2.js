71791fe9cbefd68f7f7497fa010c878028878d2e
Syed11014 <raza95114@gmail.com>
2022-10-27 11:23:09 +0500

    First Commit

---
+++ Prac2.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { read } = require('fs');
+const app = express()
+const port = 3500
+const path = require('path')
+const fs = require('fs').promises;
+
+//text
+app.get('/', (req, res) => {
+  res.send('Its Working!')
+})
+//image
+app.get('/Image', (req, res) => {
+  let imagepath = path.join(__dirname, 'img3.jpg');
+
+  res.sendFile(imagepath)
+})
+//dynamic image
+app.get('/Dynamicimage', (req, res) => {
+  imageName = req.query.imagename
+  let imagepull = path.join(__dirname, imageName)
+  
+  if (imageName == "img1.jpg" || imageName == "img2.jpg" || imageName=="download.jpg" || imageName== "img3.jpg"){
+    res.sendFile(imagepull)
+    
+  }
+  else {
+    res.send("Image not found")
+  }
+  /*fs.readFile(imageName, (err)=> {
+    res.status.end("This Image is Not Found")
+  }
+  )*/
+
+})
+
+app.listen(port, () => {
+  console.log(`Ready! ${port}`)
+}) 
